86228ead36e114460b6bf257fc4cce57e879b671
akmanrisal <akmanrisal.r@ku.th>
2021-03-15 17:30:58 +0700

    test

---
+++ 6_2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,k=0,j=0;
+    scanf("%d",&n);
+    char num[n],num1[n];
+    if(n>=4&&n<=9)
+    {
+        scanf("%s",num);
+        scanf("%s",num1);
+    }
+    else
+        printf("Error");
+
+    for(int i=0; i<n; i++)
+    {
+        if(num[i]!=num[i+1]&&num1[i]!=num1[i+1])
+        {
+            if(num[i]==num1[i])
+            {
+                k=k+1;
+            }
+            else
+                j=j+1;
+        }
+        else
+        {
+            printf("Error\n");
+            break;
+        }
+    }
+    if(k>0)
+        printf("%d %d",k,j);
+
+}
